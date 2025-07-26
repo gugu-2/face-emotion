1b40fab361e1d8d94fce3371626a198665fe3f81
Pritam x <83119264+gugu-2@users.noreply.github.com>
2025-07-26 14:25:04 +0530

    Delete backend directory

--- backend/app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Settings:
-    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:password@db:5432/emotions")
-    EMOTION_MODEL_PATH = os.getenv("EMOTION_MODEL_PATH", "./models/emotion_model.pth")
-
-settings = Settings()
